d5592f98f743010bc76797148059e65d9b4a6f75
Matthew <matthewyfy@gmail.com>
2019-01-16 17:01:59 +0800

    Resource management loading class

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,10 +1,13 @@
 #include "ResourceManager.h"
 
 void ResourceManager::doCreate(HTTPServerRequest &req, HTTPServerResponse &resp) {
+	//Print code to test endpoint
 	std::cout << "Resource Create" << std::endl;
 }
 
 void ResourceManager::doRead(HTTPServerRequest &req, HTTPServerResponse &resp) {
+
+	//Load images located in resource directory
 
 	if (req.getURI() == "/Resources/Registration/registration.html") {
 		resp.sendFile("Resources/Registration/registration.html" , "text/html");
@@ -21,9 +24,11 @@ void ResourceManager::doRead(HTTPServerRequest &req, HTTPServerResponse &resp) {
 }
 
 void ResourceManager::doUpdate(HTTPServerRequest &req, HTTPServerResponse &resp) {
+	//Print code to test endpoint
 	std::cout << "Resource Update" << std::endl;
 }
 
 void ResourceManager::doDelete(HTTPServerRequest &req, HTTPServerResponse &resp) {
+	//Print code to test endpoint
 	std::cout << "Resource Delete" << std::endl;
 }
